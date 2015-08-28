687e6f6b6c176aff4b35afdb718b849c3a4674a4
SanderSpies <sandermail@gmail.com>
2015-08-28 22:43:13 +0200

    More bugfixing

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
             'lib/layout/layoutNode-tests/*-test.js': ['browserify']
           },
           browsers: ['Chrome'],
-          singleRun: true
+            singleRun: true
         }
       }
     },
